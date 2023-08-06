ba490bf1da9a66aca96c125cefd43aaa29cff3f6
CodingLeeSeungHoon <free_minkya@naver.com>
2023-08-06 23:00:29 +0900

    Docker compose setting, application.yml setting

---
+++ needeachother/src/main/java/com/neo/needeachother/NeedeachotherApplication.java
@@ -0,0 +1,13 @@
+package com.neo.needeachother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedeachotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedeachotherApplication.class, args);
+	}
+
+}
