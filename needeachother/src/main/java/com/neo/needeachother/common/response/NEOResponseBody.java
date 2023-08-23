7847e2962befb9449e7908873b26be438c2ae8e3
CodingLeeSeungHoon <free_minkya@naver.com>
2023-08-23 09:04:54 +0900

    feat, docs : Swagger code 작성, Swagger docs 통합 목적 CustomAnnotaion 생성 (#28)

--- needeachother/src/main/java/com/neo/needeachother/common/response/NEOResponseBody.java
+++ needeachother/src/main/java/com/neo/needeachother/common/response/NEOResponseBody.java
@@ -3,6 +3,7 @@ package com.neo.needeachother.common.response;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.neo.needeachother.common.enums.NEOResponseCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,24 +22,31 @@ import java.util.List;
 @Builder
 @JsonFilter("NEOResponseJsonFilter")
 @AllArgsConstructor
+@Schema(description = "NEO의 응답 객체")
 public class NEOResponseBody<T> {
 
+    @Schema(description = "NEO의 응답이 발생한 시간", example = "2022-03-28 22:11:44")
     @Builder.Default
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private final LocalDateTime timeStamp = LocalDateTime.now();
 
+    @Schema(description = "요청이 들어온 메소드와 URI", example = "GET api/v1/users/{user_id}")
     private final String requestedMethodAndURI;
 
+    @Schema(description = "요청에 되한 최종 응답 코드", example = "SUCCESS")
     private final NEOResponseCode responseCode;
 
     /* API 요청에 대한 성공 및 실패 메시지 */
+    @Schema(description = "요청에 되한 최종 결과 메시지", example = "회원 정보를 얻어오는 데 성공했습니다.")
     private final String msg;
 
     /* Conditional Visible */
+    @Schema(description = "요청에 대한 응답 데이터")
     private final T data;
 
     /* Conditional Visible */
     /* 단순 실패 메시지가 아닌 실패 원인 */
+    @Schema(description = "요청에 대한 실패에 대한 사유 및 코드")
     private final List<NEOErrorResponse> errors;
 
 }
