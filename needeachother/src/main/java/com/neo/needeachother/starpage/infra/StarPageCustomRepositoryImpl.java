6f55ab53ecd15e68b0e760038a2d241e90f4f127
CodingLeeSeungHoon <free_minkya@naver.com>
2024-03-31 15:01:52 +0900

    feat : 조회 querydsl 임시코드 생성 (#46)

--- needeachother/src/main/java/com/neo/needeachother/starpage/infra/StarPageCustomRepositoryImpl.java
+++ needeachother/src/main/java/com/neo/needeachother/starpage/infra/StarPageCustomRepositoryImpl.java
@@ -1,12 +1,24 @@
 package com.neo.needeachother.starpage.infra;
 
+import com.neo.needeachother.starpage.domain.StarPageId;
 import com.neo.needeachother.starpage.domain.repository.StarPageCustomRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import static com.neo.needeachother.starpage.domain.QStarPage.*;
 
 @RequiredArgsConstructor
 public class StarPageCustomRepositoryImpl implements StarPageCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+
+    public void findStarPageTopViewById(StarPageId id){
+        queryFactory.select(starPage.information)
+                .from(starPage)
+                .where(starPage.starPagesId.eq(id))
+                .fetchOne();
+
+    }
 
 }
