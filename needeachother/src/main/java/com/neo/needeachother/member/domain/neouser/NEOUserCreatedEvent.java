7b86bb42df365ffdfc3dd344219d69c423d0230d
Seung Hoon Lee <free_minkya@naver.com>
2025-06-14 20:27:31 +0900

    change NEOUserCreatedEvent

--- needeachother/src/main/java/com/neo/needeachother/member/domain/neouser/NEOUserCreatedEvent.java
+++ needeachother/src/main/java/com/neo/needeachother/member/domain/neouser/NEOUserCreatedEvent.java
@@ -8,16 +8,14 @@ import java.time.LocalDateTime;
 @Getter
 public class NEOUserCreatedEvent {
     private final Long createdNeoUserId;
-    private final NEOUser user;
     private final LocalDateTime createdAt;
 
-    private NEOUserCreatedEvent(NEOUser user, Long createdNeoUserId) {
+    private NEOUserCreatedEvent(Long createdNeoUserId) {
         this.createdNeoUserId = createdNeoUserId;
-        this.user = user;
         this.createdAt = LocalDateTime.now();
     }
 
-    public static NEOUserCreatedEvent of(NEOUser user, Long createdNeoUserId) {
-        return new NEOUserCreatedEvent(user, createdNeoUserId);
+    public static NEOUserCreatedEvent of(Long createdNeoUserId) {
+        return new NEOUserCreatedEvent(createdNeoUserId);
     }
 }
